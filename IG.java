3bb7c0f7a5800dea77351201ac8a7d0734094b92
mvfao-iscteiul <mvfao@iscteiul.pt>
2019-12-09 12:37:09 +0000

    ig

---
+++ IG.java
@@ -0,0 +1,95 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.Box;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import javax.swing.JSplitPane;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+
+public class IG extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					IG frame = new IG();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public IG() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 522, 364);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnProcurar = new JButton("Procurar");
+		btnProcurar.setBounds(6, 6, 220, 29);
+		contentPane.add(btnProcurar);
+		
+		JLabel lblLoc = new JLabel("LOC");
+		lblLoc.setBounds(259, 35, 61, 16);
+		contentPane.add(lblLoc);
+		
+		textField = new JTextField();
+		textField.setBounds(299, 30, 70, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblCyclo = new JLabel("CYCLO");
+		lblCyclo.setBounds(381, 35, 61, 16);
+		contentPane.add(lblCyclo);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(439, 30, 80, 26);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblAtfd = new JLabel("ATFD");
+		lblAtfd.setBounds(259, 83, 61, 16);
+		contentPane.add(lblAtfd);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(299, 78, 70, 26);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblLaa = new JLabel("LAA");
+		lblLaa.setBounds(397, 83, 45, 16);
+		contentPane.add(lblLaa);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(439, 80, 80, 19);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		table = new JTable();
+		table.setBounds(16, 47, 210, 214);
+		contentPane.add(table);
+	}
+}
